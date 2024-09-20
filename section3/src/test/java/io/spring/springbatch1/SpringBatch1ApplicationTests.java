6acda74e880eb9e6beb43d05edf8caa4be2b3a56
ngotic <ngotic2888@gmail.com>
2024-09-21 00:11:06 +0900

    section3

---
+++ section3/src/test/java/io/spring/springbatch1/SpringBatch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.springbatch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
